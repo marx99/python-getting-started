e87304a54e4003bee6cbd9881f8d54fe9082af0a
marx99 <maxingzhe99@126.com>
2016-11-28 09:23:03 +0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -36,6 +36,30 @@ def before_todo_save(todo):
     if len(content) >= 240:
         todo.set('content', content[:240] + ' ...')
 
+def sendmail_local(subject,content):
+    sender = os.environ['sendmail_to_mail'] 
+    receiver = os.environ['sendmail_from_mail'] 
+    #subject = 'python email test'  
+    
+    smtpserver = 'smtp.126.com'  
+    username = os.environ['sendmail_from_mail']  
+    password = os.environ['sendmail_from_password']  
+    
+    #os.environ['LEANCLOUD_APP_ID']
+    
+    msg = MIMEText(content,'text','utf-8')#中文需参数‘utf-8’，单字节字符不需要  
+    msg['Subject'] = Header(subject, 'utf-8')  
+      
+    try:   
+        smtp = smtplib.SMTP()  
+        smtp.connect(smtpserver)  
+        smtp.login(username, password)  
+        smtp.sendmail(sender, receiver, msg.as_string())  
+        smtp.quit()  
+        print('sendmail OK!')
+    except smtplib.SMTPException:
+        print ("Error: sendmail error")
+
 @engine.define
 def qiandao_jd():
     
@@ -53,37 +77,6 @@ def qiandao_jd():
     jd_result = requests.get(geturl,headers = headers,verify=False)
     print(jd_result.json())  
     sendmail_local('qiandao_jd',jd_result.json())
-
-@engine.define
-def qiandao_115_marx99(**params):
-    
-    #115签到
-    
-    #大号 marx99
-    headers={
-        'Host': 'web.api.115.com',
-        'Connection': 'keep-alive',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-cn',
-        'Accept': '*/*',
-        'Origin': 'http://web.api.115.com',
-        'Content-Length': '0',
-        'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 UPad/6.2.0',
-        'Referer': 'http://web.api.115.com/bridge_2.0.html?namespace=FS.DataSrv&api=UDataAPI&_t=v5',
-        'Cookie': 'CID=8a2d83b9b714ae013f88cfdec2bcf48d; SEID=582eb96e8eee39c75f8da0595f46981d79c358e7fba1952e28a3b89e1fd62e852162005ca25fe8317b7cebba2e6a17552ca295aee34503f2c0038eaf; ssov_4158054=0_4158054_e4b7e5bba4f3176fa4343b12dd42aa3b; UID=4158054_H1_1477485814',
-    }
-    
-    url115 = 'http://web.api.115.com/user/sign'
-    s = requests.Session()
-    r1= s.post(url115,headers=headers)
-    print ('qiandao_115_marx99 ',r1.status_code,r1.json())
-        
-    geturl = 'http://web.api.115.com/user/sign?start=2016-11-01&_=1478610072924'
-    getr = requests.get(geturl,headers = headers)
-    
-    print(getr.json())
     
 @engine.define
 def qiandao_115_xiaohao(**params):
@@ -111,10 +104,12 @@ def qiandao_115_xiaohao(**params):
     r1= s.post(url115,headers=headers)
     print ('qiandao_115_135555 ',r1.status_code,r1.json())
 
-    geturl = 'http://web.api.115.com/user/sign?start=2016-11-01&_=1478610072924'
+    #geturl = 'http://web.api.115.com/user/sign?start=2016-11-01&_=1478610072924'
+    geturl = 'http://web.api.115.com/user/sign?start='+ time.strftime('%Y-%m-01') +'&_=' + str(round(time.time()*1000))
     getr = requests.get(geturl,headers = headers)
     
     print(getr.json())
+    sendmail_local('qiandao_115_135555',str(r1.json()) + str(getr.json()))
     
 @engine.define
 def qiandao_guorn(**params):
@@ -138,28 +133,80 @@ def qiandao_guorn(**params):
     else:
         print ('qiandao_guorn error:' + r1.status_code)
 
+       
+#115 qiandao
+cookies = {
+    '15942888': 'OOFA=UID=4158054_F1_1479007096; UID=4158054_F1_1479007096; CID=9cd9bb99b3101593697536c1b6a0e233; SEID=a94f79f65a4bed45cbff606930239896f115df9d81b7bafae1f34d66b87108123c35ceed4adb846076d9e1024d5c82721e2e62fcea6693104e3378ad; ssov_4158054=0_4158054_e4b7e5bba4f3176fa4343b12dd42aa3b',
+    '13555925': 'ssov_590519292=0_590519292_e41cbbf54df9d773385c0492dc79c354; OOFA=SEID=7eb6ba018b0be0e9869af0ed02957d3cc4e96181543d97ab691c2aa195349a0abb650769e034a4b0ea53c142be74ddbbb29104fcc6465c937894a842; SEID=7eb6ba018b0be0e9869af0ed02957d3cc4e96181543d97ab691c2aa195349a0abb650769e034a4b0ea53c142be74ddbbb29104fcc6465c937894a842; UID=590519292_F1_1477144725; CID=3eee667f5650c9315a1961ec7d5efcff',
+    '18640815': 'CID=75a2513cf5f55d0eb20510d701e354ed; SEID=e4bee56b359d5fbe50814a98857617657ffda3200e72e804f66a9a8e22edaf851a000ee6b79bd77e2630fb9c784bfa618700d78d5f4039bfad9ab17f; ssov_592637961=0_592637961_ed7d57ec15b95004e985cae17b31a407; UID=592637961_H1_1478431292',
+    '15694636714': 'CID=d88b040909b38a3d14c7fa78cc6032aa; SEID=354b4a3fc4f04389f3e5e5814e92f5d40e46893a0fcc55865d1de79f21ccb405acd2c1e73b9f311f1476cbeceebef8c5539f3f489923737acb6d2330; ssov_592637924=0_592637924_890a4b2e72593f4e25ee9a16149b45d1; UID=592637924_H1_1478958011'
+}
+
+def qiandao_115(userid):
+     # 签到
+    proxies = {
+        "http": "http://q:lixil@10.88.42.18:8080"
+    }
+    
+    #login
+    sign_headers = {
+        'Host': '115.com',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.4; 2014811 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36 115disk/6.2.1',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+        'Cookie': cookies[userid],
+        'X-Requested-With': 'com.ylmf.androidclient'    
+    }
+    sign_url = 'http://115.com/?ct=sign'
+    
+    sign_str = requests.get(sign_url,headers = sign_headers,proxies=proxies)
+    
+    print('sign ' + userid,sign_str.status_code)
+    
+    time.sleep(10)
+    
+    #post
+    postheaders = {
+        'Host': 'web.api.115.com',
+        'Connection': 'keep-alive',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-cn',
+        'Accept': '*/*',
+        'Origin': 'http://web.api.115.com',
+        'Content-Length': '0',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.4; 2014811 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36 115disk/6.2.1',
+        #'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 UPad/6.2.0',
+        'Referer': 'http://web.api.115.com/bridge_2.0.html?namespace=FS.DataSrv&api=UDataAPI&_t=v5',
+        'Cookie': cookies[userid]
+    }
+    
+    posturl='http://web.api.115.com/user/sign'
+    postr = requests.post(posturl,headers = postheaders,proxies=proxies)
+    print( 'post ' + userid,postr.status_code,postr.json())
+    
+    time.sleep(2)
+    
+    #get result 
+    #geturl = 'http://web.api.115.com/user/sign?start=2016-11-01&_=1478610072924'
+    geturl = 'http://web.api.115.com/user/sign?start='+ time.strftime('%Y-%m-01') +'&_=' + str(round(time.time()*1000))
+    #result output
+    get_str = requests.get(geturl,headers = postheaders,proxies=proxies)
+    print( 'get result ' + userid,get_str.status_code,get_str.json())
+    
+    #send mail
+    sendmail_local('115_qiandao:' + userid , str(postr.json()) + str(get_str.json()))
+ 
+
 @engine.define
-def sendmail_local(subject,content):
-    sender = os.environ['sendmail_to_mail'] 
-    receiver = os.environ['sendmail_from_mail'] 
-    #subject = 'python email test'  
+def qiandao_115_15694636714():
+    qiandao_115('15694636714')
     
-    smtpserver = 'smtp.126.com'  
-    username = os.environ['sendmail_from_mail']  
-    password = os.environ['sendmail_from_password']  
-    
-    #os.environ['LEANCLOUD_APP_ID']
-    
-    msg = MIMEText(content,'text','utf-8')#中文需参数‘utf-8’，单字节字符不需要  
-    msg['Subject'] = Header(subject, 'utf-8')  
-      
-    try:   
-        smtp = smtplib.SMTP()  
-        smtp.connect(smtpserver)  
-        smtp.login(username, password)  
-        smtp.sendmail(sender, receiver, msg.as_string())  
-        smtp.quit()  
-        print('sendmail OK!')
-    except smtplib.SMTPException:
-        print ("Error: sendmail error")
-        
+@engine.define
+def send_mail_test():
+    sendmail_local('title标题','虫哦你特纳那天')
